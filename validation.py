53a6064b0f1db85f982dd7644d4a4a40e2712fba
Myeongseokkang <kms00129@naver.com>
2021-11-02 21:34:46 +0900

    fetch

--- validation.py
+++ validation.py
@@ -18,16 +18,16 @@ for data in datas:
         continue
 
     #초기/현재 유동성 이더가 너무 작으면 제거
-    if((initial_Liquidity_Eth < 0.001)  and (current_Eth < 0.001)):
+    if((initial_Liquidity_Eth < 0.01)  and (current_Eth < 0.01)):
         continue
 
-    is_rugpull2 = False
-    #기존 러그풀 플래그에서 초기/현재 유동성 풀 비율을 보는 로직을 추가
-    if (data['is_rugpull']):
-        if ((current_Eth / initial_Liquidity_Eth) < 0.2):
-            is_rugpull2 = True
-    data['is_rugpull2'] = is_rugpull2
+#    is_rugpull2 = False
+#    #기존 러그풀 플래그에서 초기/현재 유동성 풀 비율을 보는 로직을 추가
+#    if (data['is_rugpull']):
+#        if ((current_Eth / initial_Liquidity_Eth) < 0.2):
+#            is_rugpull2 = True
+#    data['is_rugpull2'] = is_rugpull2
 
 
     result_datas.append(data)
-DataFrame(result_datas).to_csv('result_v1.1.csv',encoding='utf-8-sig',index=False)
+DataFrame(result_datas).to_csv('원본2.csv',encoding='utf-8-sig',index=False)
